ec0ac678de58c8f21e753283742cad21a89fd3fc
abel2606 <abel.sanchez.guerrero@gmail.com>
2024-11-27 20:57:42 -0700

    Contenido

--- scripts/formAction.js
+++ scripts/formAction.js
@@ -1,50 +1,52 @@
 const NACIONALIDADES_ACEPTADAS = [
-    {key: 'AU', name: "Australia"},
-    {key: 'BR', name: "Brasil"},
-    {key: 'CA', name: "Canadá"},
-    {key: 'CH', name: "Suiza"},
-    {key: 'DE', name: "Alemania"},
-    {key: 'DK', name: "Dinamarca"},
-    {key: 'ES', name: "España"},
-    {key: 'FI', name: "Finlandia"},
-    {key: 'FR', name: "Francia"},
-    {key: 'GB', name: "Reino Unido"},
-    {key: 'IE', name: "Irlanda"},
-    {key: 'IN', name: "India"},
-    {key: 'IR', name: "Irán"},
-    {key: 'MX', name: "México"},
-    {key: 'NL', name: "Países Bajos"},
-    {key: 'NO', name: "Noruega"},
-    {key: 'NZ', name: "Nueva Zelanda"},
-    {key: 'RS', name: "Serbia"},
-    {key: 'TR', name: "Turquía"},
-    {key: 'UA', name: "Ucrania"},
-    {key: 'US', name: "Brasil"},
+    { key: 'AU', name: "Australia" },
+    { key: 'BR', name: "Brasil" },
+    { key: 'CA', name: "Canadá" },
+    { key: 'CH', name: "Suiza" },
+    { key: 'DE', name: "Alemania" },
+    { key: 'DK', name: "Dinamarca" },
+    { key: 'ES', name: "España" },
+    { key: 'FI', name: "Finlandia" },
+    { key: 'FR', name: "Francia" },
+    { key: 'GB', name: "Reino Unido" },
+    { key: 'IE', name: "Irlanda" },
+    { key: 'IN', name: "India" },
+    { key: 'IR', name: "Irán" },
+    { key: 'MX', name: "México" },
+    { key: 'NL', name: "Países Bajos" },
+    { key: 'NO', name: "Noruega" },
+    { key: 'NZ', name: "Nueva Zelanda" },
+    { key: 'RS', name: "Serbia" },
+    { key: 'TR', name: "Turquía" },
+    { key: 'UA', name: "Ucrania" },
+    { key: 'US', name: "Brasil" },
 ];
 
 
-window.onload = function(){
+window.onload = function () {
     const form = document.getElementsByTagName("form");
     const inputs = form[0].getElementsByTagName("input");
     const selects = form[0].getElementsByTagName("select");
-    for(let input of inputs){
+    for (let input of inputs) {
         input.onfocus = resaltar;
 
         input.addEventListener('blur', noResaltar);
     }
 
-    for(let select of selects){
+    for (let select of selects) {
         select.onfocus = resaltar;
         select.addEventListener('blur', noResaltar);
     }
 
     llenarNacionalidad();
- }
+    // form[0].onsubmit = enviar;
+    form[0].onsubmit = enviar2;
+}
 
-function llenarNacionalidad(){
+function llenarNacionalidad() {
     const nacionalidad = document.getElementById("nationality");
 
-    for(let{key, name} of NACIONALIDADES_ACEPTADAS){
+    for (let { key, name } of NACIONALIDADES_ACEPTADAS) {
         const option = document.createElement("option");
         option.value = key;
         option.innerHTML = name;
@@ -54,21 +56,73 @@ function llenarNacionalidad(){
 
 
 
-function resaltar(evento){
+function resaltar(evento) {
     evento.target.classList.add("selected");
 }
 
-function noResaltar(evento){
+function noResaltar(evento) {
     const clase = evento.target.classList.contains("selected");
-    if(clase){
+    if (clase) {
         evento.target.classList.remove("selected");
     }
 }
 
 
-function resaltarDesresaltar(evento){
+function resaltarDesresaltar(evento) {
     evento.target.classList.toggle("selected");
 }
 
+function obtenerGenero() {
+    /*
+    conset gSelected = document.querySelectorAll("input[name=interest:checked");
+    */
+    const genero = document.getElementsByName("interest");
+    const gactivos = [];
+    for (let g of genero) {
+        if (g.checked) {
+            gactivos.push(g.value);
+        }
+    }
+    if (gactivos.length === 0) {
+        gactivos.push("male");
+        gactivos.push("female");
+    }
 
+    const index = gactivos.length > 1 ? Math.floor(Math.random() * 2) : 0;
+    return gactivos[index];
+}
 
+function enviar(evento) {
+    evento.preventDefault();
+    const nacionalidad = document.getElementById("nationality");
+    const genero = obtenerGenero();
+
+    window.location.href = `its-a-match.html?nac=${nacionalidad.value}&genero = ${genero}`;
+}
+
+function enviar2(evento) {
+    evento.preventDefault();
+
+    const nac = document.getElementById("nationality");
+    const genero = obtenerGenero();
+
+    const request = new Request(
+        `https://randomuser.me/api/?nat=${nac}&gender=${genero}`,
+        {
+            method: 'get',
+            headers: new Headers({
+                "Content-Type": "application/json"
+            })
+        }
+    );
+
+    fetch(request).then(function(response){
+        return response.json();
+    }).then(function(data){
+        localStorage.setItem("matchinfo", JSON.stringify(data.results[0]));
+        window.location.href = "its-a-match.html";
+
+    }).catch/function(error){
+        alert("hubo un error encoentrado a tu otra mitad: (");
+    }
+}
